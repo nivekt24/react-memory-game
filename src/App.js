d3fba379219d0c6b22270ba6e746767c4ad68bed
Kevin Tran <kvvt24@gmail.com>
2024-02-29 18:48:36 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Card from './components/Card';
 
-function App() {
+const cardImages = [
+  { src: '/img/Bulbasaur.png', matched: false },
+  { src: '/img/Charmander.png', matched: false },
+  { src: '/img/Ditto.png', matched: false },
+  { src: '/img/Mew.png', matched: false },
+  { src: '/img/Pickachu.png', matched: false },
+  { src: '/img/Squirtle.png', matched: false },
+];
+
+const App = () => {
+  const [cards, setCards] = useState([]);
+  const [flips, setFlips] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // restartGame
+  const handleRestart = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setFlips(0);
+  };
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    const handleMatchingCards = () => {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) =>
+          prevCards.map((card) =>
+            card.src === choiceOne.src ? { ...card, matched: true } : card
+          )
+        );
+        resetTurn();
+      } else {
+        setTimeout(resetTurn, 1000);
+      }
+    };
+
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      handleMatchingCards();
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices & increase flips
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setFlips((prevFlips) => prevFlips + 1);
+    setDisabled(false);
+  };
+
+  // restart game
+  useEffect(() => {
+    handleRestart();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Card</h1>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <Card
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+
+      <footer>
+        <p>Flips: {flips}</p>
+        <button onClick={handleRestart}>Restart</button>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? 'flipped' : ''}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/img/pokeball.png"
+          onClick={handleClick}
+          alt="cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
